4c7f7241ee156c74dab209bc12d9116857209245
Manuel Kugelmann <public@bitcraft.org>
2025-11-26 23:42:03 +0100

    Fix Windows PluQ build: Remove -Werror from flatcc CMakeLists.txt

--- Windows/build-pluq-mingw.sh
+++ Windows/build-pluq-mingw.sh
@@ -112,10 +112,12 @@ EOF
     mkdir -p "flatcc/build-$ARCH"
     cd "flatcc/build-$ARCH"
 
+    # Disable -Werror in flatcc by patching CMakeLists.txt
+    sed -i 's/-Werror//g' ../CMakeLists.txt
+
     cmake \
         -DCMAKE_TOOLCHAIN_FILE="$TOOLCHAIN_FILE" \
         -DCMAKE_BUILD_TYPE=Release \
-        -DCMAKE_C_FLAGS="-Wno-error=implicit-int-conversion-on-negation -Wno-error -Wno-c23-extensions -Wno-unterminated-string-initialization" \
         -DFLATCC_INSTALL=ON \
         -DFLATCC_RTONLY=ON \
         -DCMAKE_INSTALL_PREFIX="$WORK_DIR/install-$ARCH" \
